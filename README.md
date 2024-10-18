3671ef57e9066baa82db305c28f979d7fafe5bcd
qgustavor <qgustavor@users.noreply.github.com>
2024-10-18 10:37:29 -0300

    Basic app skeleton

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# bam
-Inspired by BIG-ASS MESSAGE, but open-source and with more effects
+# BAM
+
+Inspired by [BIG-ASS MESSAGE](https://bigassmessage.com), but open-source and improved.
+
+## New features
+
+- Open-source!
+- More themes!
+- Unicode support!
+- Cheaper!*
+
+* Cheaper because it can be hosted on a static web server like GitHub Pages. Shared URLs include all info about the message encoded on it. Users willing shorter URLs can use external URL shorteners like TinyURL or bit.ly. This avoid issues with message data having to be stored in someone servers.
+
+(Some might still need to be implemented, but all of those are planned.)
---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <main class="absolute w-full min-h-screen bg-gray-900 text-gray-100">
+    <div class="container mx-auto px-4">
+      <div class="flex content-center items-center justify-center h-full">
+        <div class="w-full lg:w-4/12 px-4">
+          <img src="./assets/logo.svg" alt="BAM logo" class="w-1/2 mx-auto">
+
+          <form>
+            <div class="relative w-full mb-3">
+              <label
+                class="block uppercase text-gray-300 text-xs font-bold mb-2"
+                for="message"
+                >Message
+              </label>
+              <input
+                type="email"
+                id="message"
+                class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                placeholder="Message"
+              />
+            </div>
+            <div class="relative w-full mb-3">
+              <label
+                class="block uppercase text-gray-300 text-xs font-bold mb-2"
+                for="style"
+                >Style
+              </label>
+              <select
+                id="style"
+                class="border-0 px-3 py-3 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+              >
+                <option
+                  v-for="(styleName, styleKey) in styles"
+                  :value="styleKey"
+                >{{ styleName }}
+                </option>
+              </select>
+            </div>
+            <div class="text-center mt-6">
+              <button
+                class="bg-gray-100 text-gray-900 active:bg-gray-300 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-1/3"
+              >
+                Preview
+              </button>
+              <button
+                class="bg-gray-100 text-gray-900 active:bg-gray-300 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-1/3"
+              >
+                Share URL
+              </button>
+            </div>
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+const styles = {
+  basic: 'Basic',
+  magic: 'Magic',
+  jprdy: 'JPRDY',
+  heart: 'Heart',
+  hypno: 'Hypno'
+}
+</script>
+
+<style>
+html {
+  color-scheme: dark;
+}
+</style>
