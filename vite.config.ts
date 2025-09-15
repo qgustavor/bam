24036226d63533f62c913cd48ffa51d047f351c9
qgustavor <qgustavor@users.noreply.github.com>
2025-09-15 15:26:48 -0300

    Tailwind migration - Part 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/bam/',
   plugins: [
     vue(),
+    tailwindcss(),
     VitePWA({
       manifest: {
         name: 'BAM!',
