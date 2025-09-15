dba2575babb696665dde766be08d437802531399
qgustavor <qgustavor@users.noreply.github.com>
2025-09-15 14:19:14 -0300

    Make it a PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/bam/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: 'BAM!',
+        short_name: 'BAM!',
+        description: 'BAM! A Big-Ass Message Inspired Web App!',
+        theme_color: '#111827',
+        display: 'fullscreen'
+      },
+      pwaAssets: {
+        image: 'public/logo.svg'
+      }
+    })
+  ]
 })
